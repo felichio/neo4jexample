dec001e0db13e164569fe810508ba9e64478f71d
felichio <fsafaridis@hotmail.com>
2022-06-23 22:21:07 +0300

    comments

--- server/Model/Query2.cs
+++ server/Model/Query2.cs
@@ -18,7 +18,7 @@ namespace server
         
 
 
-        // POST query2/{start: <start>, end: <end>} returns JSON
+        // POST query2 returns JSON
         public static string query = "WITH apoc.date.parse($start, 'ms', 'yyyy-MM-dd HH:mm:ss') AS start, apoc.date.parse($end, 'ms', 'yyyy-MM-dd HH:mm:ss') AS end MATCH (t:Transaction)-[:PAYS]->(o:Output) WHERE t.time >= datetime({epochmillis: start}) AND t.time <= datetime({epochmillis: end}) RETURN collect(o.recipient) AS recipients, sum(o.value) AS total_value, sum(o.value_usd) AS total_value_usd;";
         
     }
--- server/Model/Query9.cs
+++ server/Model/Query9.cs
@@ -15,7 +15,7 @@ namespace server
         public int consecutive_days {get; set;}
 
 
-        // POST query8 returns JSON
+        // POST query9 returns JSON
         public static string query = "match (o:Output)-[:SPENDS]->(i:Input) with o.recipient as recipient, o.time as day order by day asc with recipient, collect(distinct date(day)) as days with recipient, reduce(acc = [], k in range(0, size(days) -2) | case WHEN (duration.inDays(days[k], days[k + 1]).days) = 1 THEN acc + 1 ELSE acc + 0 END) as seq call apoc.coll.split(seq, 0) yield value as seqs with recipient, max(size(seqs)) as longest_streak where longest_streak >=$K return recipient, longest_streak + 1 as consecutive_days;";
         
     }
