15e2a31e070793e6f45f6877bd62efe2aa07f153
felichio <fsafaridis@hotmail.com>
2022-06-24 18:30:45 +0300

    add log for rels

--- inserter/model/Relationships.cs
+++ inserter/model/Relationships.cs
@@ -19,6 +19,7 @@ namespace inserter
             
             try
             {
+                Console.WriteLine("Making Block<-[:VALIDATED_FROM]-Transaction relationships");
                 result = await session.WriteTransactionAsync(async tx => {
                     var reader = await tx.RunAsync("MATCH (b:Block), (t:Transaction) WHERE b.id = t.block_id CREATE (b)<-[:VALIDATED_FROM]-(t);");
                     return await reader.ConsumeAsync();
@@ -39,6 +40,7 @@ namespace inserter
             
             try
             {
+                Console.WriteLine("Making Transaction<-[:FEEDS]-Input relationships");
                 result = await session.WriteTransactionAsync(async tx => {
                     var reader = await tx.RunAsync("MATCH (t:Transaction), (i:Input) WHERE t.hash = i.spending_transaction_hash CREATE (t)<-[:FEEDS]-(i);");
                     return await reader.ConsumeAsync();
@@ -59,6 +61,7 @@ namespace inserter
             
             try
             {
+                Console.WriteLine("Making Output<-[:PAYS]-Transaction relationships");
                 result = await session.WriteTransactionAsync(async tx => {
                     var reader = await tx.RunAsync("MATCH (t:Transaction), (o:Output) WHERE t.hash = o.transaction_hash CREATE (t)-[:PAYS]->(o);");
                     return await reader.ConsumeAsync();
@@ -79,6 +82,7 @@ namespace inserter
             
             try
             {
+                Console.WriteLine("Making Input<-[:SPENDS]-Output relationships");
                 result = await session.WriteTransactionAsync(async tx => {
                     var reader = await tx.RunAsync("MATCH (o:Output), (i:Input) WHERE o.transaction_hash = i.transaction_hash and o.recipient = i.recipient and o.index = i.index CREATE (o)-[:SPENDS]->(i);");
                     return await reader.ConsumeAsync();
@@ -99,6 +103,7 @@ namespace inserter
             
             try
             {
+                Console.WriteLine("Making GuessedMiner<-[:GUESSED_FROM]-Block relationships");
                 result = await session.WriteTransactionAsync(async tx => {
                     var reader = await tx.RunAsync("MATCH (b:Block) MERGE (g:GuessedMiner {name: b.guessed_miner}) MERGE (b)-[:GUESSED_FROM]->(g);");
                     return await reader.ConsumeAsync();
